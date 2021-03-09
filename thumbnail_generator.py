ea0781ee85945ead0a5d1bf902669cf8928ecc7b
Yuri Delgado <yuricd@bol.com.br>
2021-03-09 19:47:16 -0300

    Apply wrapped functions and color selection

--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -7,40 +7,37 @@
 # <span>Photo by <a href="https://unsplash.com/@sharonmccutcheon?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sharon McCutcheon</a> on <a href="https://unsplash.com/s/photos/money?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
 from gimpfu import *
 import os
-
-def log(msg):
-    pdb.gimp_message(msg)
-
-def thumb_generator(background_path, picture_path, first_line, second_line, third_line, font_face) :
+import utils as my
+    
+def thumb_generator(background_path, picture_path, first_line, second_line, third_line, font_face, color=(0.0, 0.0, 0.0)):
+    
+    pdb.gimp_context_set_foreground(color)
 
     # Create an show new image window with background
-    img = pdb.gimp_file_load(background_path, background_path)
-    gimp.Display(img)
+    img = my.load_file(background_path)
     gimp.displays_flush()
     gimp.context_push()
-    
-    img.undo_group_start()
-    
+        
     # Add overlay
-    overlay = gimp.Layer(img, 'overlay', img.width, img.height, RGB_IMAGE, 35, NORMAL_MODE)
+    overlay = my.create_layer(image=img, width=img.width, height=img.height, image_type=RGB_IMAGE, 
+                           name='overlay', opacity=35, mode=NORMAL_MODE, add_to_img=False)
     overlay.fill(FILL_WHITE)
-    img.add_layer(overlay, 0)
+    my.add_layer(img, overlay)
     
     # Add bottom rectangle
     bottom_rect_h = int(round(img.height * .09))
-    bottom_rect_layer = gimp.Layer(img, 'bottom_rect', img.width, bottom_rect_h, RGB_IMAGE, 100, NORMAL_MODE)
-    pdb.gimp_image_insert_layer(img, bottom_rect_layer, None, -1)
-    bottom_rect_layer.translate(0, img.height - bottom_rect_h)
+    bottom_rect_layer = my.create_layer(image=img, width=img.width, height=bottom_rect_h, image_type=RGB_IMAGE, 
+                                     name='bottom_rect', opacity=100, mode=NORMAL_MODE)
+    pdb.gimp_drawable_edit_fill(bottom_rect_layer,FILL_FOREGROUND)
+    my.translate_layer(bottom_rect_layer, 0, img.height - bottom_rect_h)
     
-    # Load guy
+    # Load popping out
     if (picture_path):
-        picture_layer = pdb.gimp_file_load_layer(img, picture_path)
-        img.add_layer(picture_layer, 0)
+        picture_layer = my.load_image(img, picture_path)
         picture_w = int(round(img.width) * .47)
-        picture_h = int(round(float(picture_layer.height) * (float(picture_w) / float(picture_layer.width))))
-        pdb.gimp_layer_scale(picture_layer, picture_w, picture_h, False) # false to keep the same coord
-        picture_layer.translate(img.width - picture_w, img.height - picture_h)
-        pdb.script_fu_drop_shadow(img, picture_layer, -4, -4, 20, "#000000", 90, FALSE)
+        my.scale_layer(picture_layer, picture_w, keep_proportion=True)
+        my.translate_layer(picture_layer, img.width - picture_w, img.height - picture_layer.height)
+        pdb.script_fu_drop_shadow(img, picture_layer, -4, -4, 20, '#000000', 90, FALSE)
         
     # Create the text lines
     font_size_big = img.height * .24
@@ -48,17 +45,19 @@ def thumb_generator(background_path, picture_path, first_line, second_line, thir
     margin_top = img.height / 7
     margin_left = img.width / 25
     
-    first_line_layer = pdb.gimp_text_fontname(img, None, margin_left, margin_top, first_line, 0, True, font_size_big, PIXELS, font_face)
+    first_line_layer = my.add_text(image=img, x=margin_left, y=margin_top, content=first_line, 
+                                font_size=font_size_big, font_face=font_face)
     
-    second_line_layer = pdb.gimp_text_fontname(img, None, margin_left, margin_top + font_size_big, ' ' + second_line + ' ', 0, True, font_size_ordinary, PIXELS, font_face)
-    pdb.gimp_text_layer_set_color(second_line_layer, '#FFF')
+    second_line_layer = my.add_text(image=img, x=margin_left, y=margin_top + font_size_big, 
+                                 content=' ' + second_line + ' ',  font_size=font_size_ordinary, font_face=font_face)
+    my.set_layer_text_color(second_line_layer, '#FFF')
     pdb.gimp_selection_all(img)
     pdb.gimp_edit_bucket_fill(second_line_layer, BUCKET_FILL_FG, LAYER_MODE_BEHIND, 100, 0, False, 0, 0)
     
     tl_top = margin_top + font_size_big + font_size_ordinary
-    third_line_layer = pdb.gimp_text_fontname(img, None, margin_left, tl_top, third_line, 0, True, font_size_ordinary, PIXELS, font_face)
+    third_line_layer = my.add_text(image=img, x=margin_left, y=tl_top, content=third_line, 
+                                font_size=font_size_ordinary, font_face=font_face)
     
-    img.undo_group_end()
     gimp.context_pop()
 
 register(
@@ -77,6 +76,7 @@ register(
         (PF_STRING, 'second_line', 'Second line', 'LAKES TO VISIT'),
         (PF_STRING, 'third_line', 'Third line', 'IN SOUTH AMERICA'),
         (PF_FONT, 'font_face', '* Font face', 'Londrina Solid Heavy'),
+        (PF_COLOR, "color", "Color", (0.0, 0.0, 0.0))
     ],
     [],
     thumb_generator, menu="<Image>/File")
